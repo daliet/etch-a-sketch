3681b1319c4f2a7be0d3da51ce719b00aa43c0e7
P Gkounis <pgkounis@outlook.com>
2022-07-11 22:25:48 +0100

    Move event handlers outside the loop

--- game.js
+++ game.js
@@ -1,34 +1,41 @@
 const container =  document.querySelector('#container');
 const btn = document.querySelector('#btn');
+
 btn.addEventListener('click', () =>
 addGrid());
 
+function addColor(){
+
+  let squares = container.querySelectorAll('.square');
+
+    squares.forEach(square => {
+      square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = 'black';
+    });
+    });
+    
+    squares.forEach(square => {
+      square.addEventListener('mouseleave', () => {
+      square.style.backgroundColor = 'black';
+    }); 
+    });
+}
+
 function addGrid(){  
 
-let input = prompt("Please enter the number of squares per side");
-container.style.cssText = 'grid-template-rows: repeat('+input+' , 1fr); grid-template-columns: repeat('+input+', 1fr)';
+  let content = document.createElement('div');  
+  let input = prompt("Please enter the number of squares per side (2 - 100)");
 
-let content = document.createElement('div');
- 
- for (let i = 0; i <= input * input; i++){
-  
-    content = document.createElement('div'); 
-    content.classList.add('square');
-    squares = container.querySelectorAll('.square')
-    container.appendChild(content);
-        
-      squares.forEach(square => {
-        square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'black';
-       });
-      });
-       
-      squares.forEach(square => {
-        square.addEventListener('mouseleave', () => {
-         square.style.backgroundColor = 'black';
-       }); 
-      });
-      
+    while((input == null || input > 100 || input < 2 || isNaN(input))){
+      input = prompt ("Please enter the number of squares per side (2 - 100)");
     }
-    return content; 
-  }  
+
+    for (let i = 0; i < input * input; i++){
+
+        container.style.cssText = 'grid-template-rows: repeat('+input+' , 1fr); grid-template-columns: repeat('+input+', 1fr)';
+        content = document.createElement('div'); 
+        content.classList.add('square');
+        container.appendChild(content);        
+    }
+        addColor(); 
+}  
